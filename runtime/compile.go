d1e08775d225e15726d93c04547d62f7e0d29a5a
makachanm <hyeonseongyang09@gmail.com>
2025-08-12 11:31:26 +0900

    (add): compiler

--- runtime/compile.go
+++ runtime/compile.go
@@ -1,9 +1,13 @@
 package runtime
 
-import "cutter/parser"
+import (
+	"cutter/parser"
+	"fmt"
+)
 
 func compileCall(call parser.CallObject) []VMInstr {
 	var instrs []VMInstr
+	// 값 인자 push
 	for _, arg := range call.Args {
 		switch arg.Type {
 		case parser.INTGER:
@@ -16,8 +20,31 @@ func compileCall(call parser.CallObject) []VMInstr {
 			instrs = append(instrs, VMInstr{Op: OpPushBool, Arg: arg.BoolData})
 		}
 	}
+	// CallableArgs(중첩 함수 호출)도 재귀적으로 변환
+	for _, subcall := range call.CallableArgs {
+		instrs = append(instrs, compileCall(subcall)...)
+	}
+	// 함수 호출
 	instrs = append(instrs, VMInstr{Op: OpCall, Arg: call.Name})
 	return instrs
+}
+
+func compileFuncDef(fun parser.FunctionObject) []VMInstr {
+	// 함수 정의를 OpSetFunc으로 등록
+	// 인자 이름 추출
+	argNames := make([]string, 0, len(fun.Args))
+	for _, arg := range fun.Args {
+		argNames = append(argNames, arg.Name)
+	}
+	// 함수 본문(여기선 FuncBodys) 컴파일
+	bodyInstrs := compileCall(fun.FuncBodys)
+	return []VMInstr{
+		{
+			Op:   OpSetFunc,
+			Arg:  fun.Name,
+			Args: []interface{}{argNames, bodyInstrs},
+		},
+	}
 }
 
 func CompileASTToVMInstr(ast parser.HeadNode) []VMInstr {
@@ -25,11 +52,13 @@ func CompileASTToVMInstr(ast parser.HeadNode) []VMInstr {
 	for _, body := range ast.Bodys {
 		switch body.Type {
 		case parser.FUCNTION_DEFINITION:
-			// 함수 정의는 환경에 등록 (여기선 생략)
+			instrs = append(instrs, compileFuncDef(body.Func)...)
 		case parser.FUNCTION_CALL:
 			instrs = append(instrs, compileCall(body.Call)...)
 		case parser.NORM_STRINGS:
 			instrs = append(instrs, VMInstr{Op: OpPushString, Arg: body.Norm.Data})
+		default:
+			fmt.Println("Unknown BodyObject type in CompileASTToVMInstr")
 		}
 	}
 	return instrs
